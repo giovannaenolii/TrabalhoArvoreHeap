eaa41c461ce186d6181f41fcdd19d60ba582c48e
Giovanna Oliveira <giovannahenriquehg@gmail.com>
2026-06-09 12:03:22 -0300

    Create No class with a constructor and toString method

---
+++ No.java
@@ -0,0 +1,12 @@
+public class No {
+    int valor;
+
+    public No(int valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(valor);
+    }
+}
